cc11ce8b9b05fe484de6f59d604b7557814f40e6
Vikrant-kun <vikrantvinchurkar12@gmail.com>
2026-05-13 15:27:31 +0530

    learned Custom Type Declaration

--- projects/cards/main.go
+++ projects/cards/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-    var card string = "Ace of Spades"
-	fmt.Println(card)
+    cards := deck{"Ace of Spades", newCard()}
+    cards = append(cards, "Six of Hearts")
+
+	cards.print()
+}
+
+func newCard() string {
+    return "Five of Diamonds"
 }
