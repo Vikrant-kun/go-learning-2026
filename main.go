f80d7d5bee6722bb144537d129798b5ae47eb1bf
Vikrant-kun <vikrantvinchurkar12@gmail.com>
2026-05-29 10:04:20 +0530

    Maps - cpmpleted

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	m := map[string][]int{"a": {1, 2, 3}}
-	fmt.Println(m)
-}
+	mp := map[uint]uint{}
+	n := uint(100)
 
+	for number := uint(1); number <= n; number++ {
+		for d := uint(1); d <= 5; d ++ {
+			if number % d == 0 {
+				mp[d]++
+			}
+		}
+	}
+	fmt.Print(mp)
+
+}
+ 
