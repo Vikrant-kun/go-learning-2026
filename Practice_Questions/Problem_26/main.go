fea12ec343d11f5097460f4833d0ca94bc1120e3
Vikrant-kun <vikrantvinchurkar12@gmail.com>
2026-07-11 15:26:49 +0530

    Add initial implementation for counting divisors in a range

---
+++ Practice_Questions/Problem_26/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mp := map[uint]uint{}
+	n := uint(100)
+
+	for number := uint(1); number <= n; number ++ {
+		for d := uint(1); d <= 5; d++ {
+			if number%d == 0 {
+				mp[d]++
+			}
+		}
+	}
+	fmt.Println(mp)
+}
